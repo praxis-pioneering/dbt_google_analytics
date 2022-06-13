d9e3799fe86507fa908c66a3afb9fec50177a75a
ecal <ethan@ecal.dev>
2022-06-13 11:43:27 -0400

    cleanup

--- models/marts/dim_product_performance.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  *, 
-  sum(revenue/nullif(quantity,0)) as avgRev,
-  sum(quantity/nullif(transactions,0)) as avgQuantity 
-FROM {{ref('stg_ga__revenue')}}
-GROUP BY 1,2,3,4
-ORDER BY 2 DESC
