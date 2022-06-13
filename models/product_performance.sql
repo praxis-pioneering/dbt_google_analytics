abf62afbf9b5a338f7333062d23056bdc9a692de
ecal <ethan@ecal.dev>
2022-06-13 13:22:40 -0400

    refunds, conditional product variant

--- models/product_performance.sql
+++ models/product_performance.sql
@@ -6,15 +6,17 @@ with
 
 pivot_and_aggregate_sessions_to_product_level as (
 	select
-		product_name,
-		product_sku,
-        product_variant,
+		product_name as name,
+		product_sku as sku,
+        if (product_variant != '(not set)', product_variant, null) as variant,
         avg(product_price) / {{price_divisor}} as price,
         countif(action_type = 'purchase') as purchases,
         sum(product_revenue/{{price_divisor}}) as revenue,
         count(distinct if(action_type = 'view', client_id, null)) as num_users_viewed,
         countif(action_type = 'view') as total_views,
         (countif(action_type = 'purchase') / nullif(countif(action_type = 'view'), 0)) as conversion_rate
+        countif(action_type = 'refund') as refunds,
+        sum(product_refund_amount/{{price_divisor}}) as total_refund_amount,
 	from sessions
     {{ group_by_first(3) }}
     order by 2
