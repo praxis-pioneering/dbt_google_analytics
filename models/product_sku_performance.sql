b6021bb1407b321c338c9dfa3b59d59dc8558682
Ethan Callanan <ethan@praxispioneering.com>
2022-06-15 20:37:04 +0000

    cleanup product_sku_performance

--- models/product_sku_performance.sql
+++ models/product_sku_performance.sql
@@ -2,20 +2,18 @@ with
 
 product_sku_performance as (
 	select
-		utc_hour,
-		name,
+		time,
+		product_name,
 		variant,
 		sku,
-		price,
 		purchases,
 		revenue,
 		num_users_viewed,
-		total_views,
-        (purchases / nullif(total_views, 0)) as conversion_rate,
+		views,
+        safe_divide(purchases, views) as conversion_rate,
 		refunds,
 		total_refunded_amount,
 	from {{ ref('product_variants') }}
-    order by sku
 )
 
 select * from product_sku_performance
