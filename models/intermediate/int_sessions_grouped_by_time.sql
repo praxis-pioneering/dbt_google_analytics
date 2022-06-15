174d6f2b3bd3621b365db2c50fc10badcd7b676d
Ethan Callanan <ethan@praxispioneering.com>
2022-06-15 18:42:53 +0000

    count actions in int model

--- models/intermediate/int_sessions_grouped_by_time.sql
+++ models/intermediate/int_sessions_grouped_by_time.sql
@@ -15,6 +15,10 @@ sessions_grouped_by_time as (
 		product_refund_amount,
 		array_agg(client_id) as client_ids,
 		array_agg(action) as actions,
+		countif(action = 'view') as views,
+		count(distinct if(action = 'view', client_id, null)) as num_users_viewed,
+		countif(action = 'purchase') as purchases,
+		countif(action = 'refund') as refunds,
 		countif(is_direct) as ga_direct_sessions,
 		array_agg(channel) as channels,
 		array_agg(medium) as mediums
@@ -23,3 +27,4 @@ sessions_grouped_by_time as (
 )
 
 select * from sessions_grouped_by_time
+where sku = '838532832'
